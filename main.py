1a88cf993677df62701e5becf8b245de49c7b09b
Data Engineer 01 <abhinav.verma@flyhomes.com>
2023-05-26 19:30:33 +0530

    ADWH-170
    ADWH-170

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ more_binary_data = b'Here we have some more data'
 
 # Method 1: Object.put()
 s3 = boto3.resource('s3')
-object = s3.Object('flyhomes-dwh-landing-dev/Docker', 'filename_1.txt')
+object = s3.Object('flyhomes-dwh-landing-dev', 'Docker/filename_1.txt')
 object.put(Body=some_binary_data)
